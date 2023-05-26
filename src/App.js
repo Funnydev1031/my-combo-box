f319a07599801cc10fdcf2fc60c4d15d6b8a3f7a
colin <happydev0728@gmail.com>
2023-05-26 06:56:07 +0100

    initialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ComboBox from "./components/ComboBox";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Creative Combo Box</h1>
+      <ComboBox />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  test("renders the combo box component", () => {
+    render(<App />);
+    const comboBoxElement = screen.getByTestId("combo-box");
+    expect(comboBoxElement).toBeInTheDocument();
+  });
+
+  test("selects an option from the combo box", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText("Select an option");
+
+    fireEvent.click(inputElement);
+    const optionElement = screen.getByText("Apple");
+    fireEvent.click(optionElement);
+
+    expect(inputElement.value).toBe("Apple");
+  });
+});
